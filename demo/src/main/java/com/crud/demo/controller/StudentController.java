8da19eb94fcd06e48f67605579ded89a1a01e3fb
Dinesh korukonda <dineshkorukonda05@gmail.com>
2025-02-19 12:12:47 +0530

    added full app

---
+++ demo/src/main/java/com/crud/demo/controller/StudentController.java
@@ -0,0 +1,49 @@
+package com.crud.demo.controller;
+
+import com.crud.demo.model.Student;
+import com.crud.demo.repository.StudentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        logger.info("Fetching all students");
+        return studentRepository.findAll();
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentRepository.findById(id).orElseThrow();
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student studentDetails) {
+        Student student = studentRepository.findById(id).orElseThrow();
+        student.setName(studentDetails.getName());
+        student.setEmail(studentDetails.getEmail());
+        return studentRepository.save(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentRepository.deleteById(id);
+    }
+}
